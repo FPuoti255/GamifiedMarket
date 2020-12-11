099ee15f709e6eded1cbbbeb34f72d626d204ffa
Elia <10609885@polimi.it>
2020-12-11 18:44:15 +0100

    added todos for tonight

--- src/main/java/controllers/HelloWorld.java
+++ src/main/java/controllers/HelloWorld.java
@@ -11,11 +11,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "helloServlet", value = "/hello")
+// todo: refactor this servlet and remove "hello world" template
+@WebServlet(name = "helloServlet", value = "/hello") // fraternali uses this tag in this way: @WebServlet("/hello")
 public class HelloWorld extends HttpServlet {
     private String message;
 
-    @EJB(beanName = "ProductService")
+    @EJB(beanName = "ProductService") // "beanName" should be "name" and should include path from the module directory
     private ProductService pdrservice;
 
     public void init() {
--- src/main/java/services/ProductService.java
+++ src/main/java/services/ProductService.java
@@ -6,10 +6,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 
-@Stateless(name = "ProductService")
+@Stateless(name = "ProductService") // should not even provide the name
 public class ProductService {
-    @PersistenceContext(name = "gamified_market")
-    EntityManager em;
+    @PersistenceContext(name = "gamified_market") // "name" should be "unitName"
+    EntityManager em; // is it private? 
 
     public ProductService() {
     }
