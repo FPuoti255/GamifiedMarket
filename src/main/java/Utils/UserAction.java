efb1c6aba325a01bc96f14b74c9544fbe21c8814
Elia Ravella <10609885@polimi.it>
2021-02-05 21:30:58 +0100

    added (jacoco) enum for user log

---
+++ src/main/java/Utils/UserAction.java
@@ -0,0 +1,66 @@
+package Utils;
+
+public enum UserAction {
+    SUBMITTED (0,"submitted"),
+    CANCELLED (1, "cancelled"),
+    LOGIN (2, "login"),
+    LOGOUT (3, "logout"),
+    LEAVE_REVIEW (4, "leave_review"),
+    BANNED (5, "banned");
+
+    private final int num;
+    private final String name;
+
+    UserAction(int i, String name) {
+        this.num = i;
+        this.name = name;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static UserAction parseName (String action) throws IllegalArgumentException {
+        switch (action.toLowerCase()){
+            case "submitted" :
+                return SUBMITTED;
+            case "cancelled" :
+                return CANCELLED;
+            case "login" :
+                return LOGIN;
+            case "logout" :
+                return LOGOUT;
+            case "leave_review" :
+                return LEAVE_REVIEW;
+            case "banned" :
+                return BANNED;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    public static UserAction parseInt (int action) throws IllegalArgumentException {
+        switch (action){
+            case 0 :
+                return SUBMITTED;
+            case 1 :
+                return CANCELLED;
+            case 2 :
+                return LOGIN;
+            case 3 :
+                return LOGOUT;
+            case 4 :
+                return LEAVE_REVIEW;
+            case 5 :
+                return BANNED;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+
+}
