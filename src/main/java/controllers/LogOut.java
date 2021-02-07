0b68f494332ee16bed87c523883abcce0cc176ee
FPuoti255 <fpuoti99@gmail.com>
2021-02-07 22:38:43 +0100

    Log out functionality implemented. Navbar included into user html file. You have still to add it in yours

---
+++ src/main/java/controllers/LogOut.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LogOut", value = "/LogOut")
+public class LogOut extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+
+        response.sendRedirect("index.html");
+    }
+
+}
